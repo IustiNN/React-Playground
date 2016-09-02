aa91ec9d32fbe5c44d090255900d43808c1e1c0f
IustiNN <iustin.nitza@gmail.com>
2016-09-02 12:31:07 +0300

    more stuff

--- src/js/CommentBox.js
+++ src/js/CommentBox.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import CommentList from './CommentList';
+import CommentForm from './CommentForm';
 
 class CommentBox extends React.Component {
   render() {
     return (
         <div className="commentBox">
-        	Hello, world! I am a CommentBox.
+        	<h1>Hello, world! I am a CsodsmmentBox.</h1>
+        	<CommentList />
+
+        	<CommentForm />
         </div>
       )
   }
