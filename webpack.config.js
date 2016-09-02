bbcd7f2a3a0511e19d68857db2f71c38d4b8e953
IustiNN <iustin.nitza@gmail.com>
2016-09-02 12:32:39 +0300

    change structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-	entry: './main.js',
-	output: {
-		path: './',
-		filename: 'index.js'
-	},
-	devServer: {
-		inline: true,
-		port: 3333
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: '/node_modules/',
-				loader: 'babel',
-				query: {
-					presets: ['es2015', 'react']
-				}
-			}
-		]
-	}
-}
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
