15a9aef5b14e1a2d551d7f725c0edbca233bd316
iustin <iustin.nitza@gmail.com>
2016-10-15 12:39:40 +0300

    Move all navigation html to a new Nav component

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavLink from '../components/NavLink'
-import Footer from '../components/Footer'
+import Nav from '../components/layout/Nav'
+import Footer from '../components/layout/Footer'
 import '../../App.css'
 
 
@@ -10,28 +10,12 @@ export default class Layout extends React.Component {
 		console.log(history.isActive("articles"));
 		return (
 			<div>
-				<div className="ui large top fixed hidden menu">
-					<div className="ui container">
-					<NavLink onlyActiveOnIndex className="item" to="/">Home</NavLink>
-					<NavLink className="item" to="/work">Work</NavLink>
-					<NavLink className="item" to="/company">Company</NavLink>
-					<NavLink className="item" to="/articles">Articles</NavLink>
-					<div className="right menu">
-						<div className="item">
-						<a className="ui button">Log in</a>
-						</div>
-						<div className="item">
-						<a className="ui primary button">Sign Up</a>
-						</div>
-					</div>
-					</div>
-				</div>
-				{this.props.children}		
-				<Footer />	
+				<Nav />
+				{this.props.children}
+				<Footer />
 			</div>
-			
+
 		)
 
-		
 	}
 }
